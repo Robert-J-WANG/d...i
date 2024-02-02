e4eaafd72d6dd521a168273fd8e327c29462e8e5
Robert-J-WANG <robin.nz420@gmail.com>
2024-02-02 21:21:14 +1300

    01. jQuery 购物车练习

--- 3. 前端高薪工程师/01. 技术提升/10 第三方库/01. jQuery/练习/index.js
+++ 3. 前端高薪工程师/01. 技术提升/10 第三方库/01. jQuery/练习/index.js
@@ -3,8 +3,10 @@ const JqueryObjs = {
   checkAlls: $(".checkAll"),
   checkItems: $(".checkItem"),
   checkBoxes: $("[type=checkbox]"),
+  nums: $(".nums"),
+  sums: $(".sums"),
 };
-const { checkAlls, checkItems, checkBoxes } = JqueryObjs;
+const { checkAlls, checkItems, checkBoxes, nums, sums } = JqueryObjs;
 
 /* ----------------------- 全选按钮勾选 ----------------------- */
 checkAlls.change(function () {
@@ -15,14 +17,12 @@ checkAlls.change(function () {
       checked,
     });
   });
+  setPrice();
 });
 /* ----------------------- 单选框勾选 ---------------------- */
 checkItems.change(function () {
-  // 获取选中的单选框数组
-  const checkeds = $.grep(checkItems, (item) => {
-    return $(item).prop("checked");
-  });
-  console.log(checkeds);
+  const checkeds = getCheckedItens();
+  setPrice();
   // 设置全选框状态
   checkAlls.each((index, item) =>
     $(item).prop({
@@ -30,3 +30,30 @@ checkItems.change(function () {
     })
   );
 });
+/* ---------------------- 计算商品数量和价格 --------------------- */
+function getCheckedItens() {
+  // 获取选中的单选框数组
+  return $.grep(checkItems, (item) => $(item).prop("checked"));
+}
+function setPrice() {
+  const checkeds = getCheckedItens();
+  const totalNum = checkeds.length;
+  // 设置每个item的价格
+  const prices = checkeds.map((item) => {
+    const priceDiv = $(item).parents(".item").children(".price");
+    const unitPrice = $(priceDiv).children("em").text().replace("￥", "");
+    const numDiv = priceDiv.next();
+    const number = $(numDiv).children("input").val();
+    const itemPrice = (+unitPrice * number).toFixed(2);
+    const sumDiv = numDiv.next();
+    sumDiv.children("em").text(`￥${itemPrice}`);
+    return itemPrice;
+  });
+
+  // 设置总价
+  const totalPrice = prices.reduce((a, b) => a + +b, 0);
+  // const totalPrice = checkeds.reduce((item) => {});
+  console.log(totalNum, totalPrice);
+  nums.children("em").text(totalNum);
+  sums.children("em").text(`￥${totalPrice.toFixed(2)}`);
+}
