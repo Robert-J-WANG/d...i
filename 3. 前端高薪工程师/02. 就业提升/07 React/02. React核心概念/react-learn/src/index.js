6935e38d19c508696f6b49076f1bf3fce343b110
Robert-J-WANG <robin.nz420@gmail.com>
2024-03-01 15:49:24 +1300

    02/11. 传递元素内容

--- 3. 前端高薪工程师/02. 就业提升/07 React/02. React核心概念/react-learn/src/index.js
+++ 3. 前端高薪工程师/02. 就业提升/07 React/02. React核心概念/react-learn/src/index.js
@@ -1,7 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App'
+import Comp from "./Comp"
 
 ReactDOM.render((
-    <App/>
+    <Comp content1={<h2>第2组元素内容</h2>} content2={<h2>第3组元素内容</h2>}>
+
+        <h2>第1组元素内容</h2>
+
+    </Comp>
 ), document.getElementById('root'));
