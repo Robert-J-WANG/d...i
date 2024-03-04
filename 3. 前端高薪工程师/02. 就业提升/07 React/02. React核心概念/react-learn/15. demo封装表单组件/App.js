9de51259242ee2f0ad4b52d95f774a08bd3d8a38
Robert-J-WANG <robin.nz420@gmail.com>
2024-03-04 18:06:14 +1300

    03/01. 属性默认值和类型检查

---
+++ 3. 前端高薪工程师/02. 就业提升/07 React/02. React核心概念/react-learn/15. demo封装表单组件/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Test from "./components/common/CheckboxGroup/Test";
+import RadioTest from "./components/common/RadioGroup/RadioTest";
+import SelectTest from "./components/common/SelectGroup/SelectTest";
+
+export default function App() {
+  return (
+    <div>
+      <Test />
+      <RadioTest />
+      <SelectTest />
+    </div>
+  );
+}
